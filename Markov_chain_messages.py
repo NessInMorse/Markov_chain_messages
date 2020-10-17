4ec4c7bed2a52b5d2f220aee2118341a2c6be26b
NessInMorse <56824499+NessInMorse@users.noreply.github.com>
2020-10-17 13:55:29 +0200

    Update Markov_chain_messages.py

--- Markov_chain_messages.py
+++ Markov_chain_messages.py
@@ -192,10 +192,10 @@ def makeNewchats(c_choice):
 def main():
         openfile()
         file=open("analysis.txt","a",encoding='utf-8')
-        file.write(f"\n\n{asctime(localtime(time()))}____________________________________________________________")
+        file.write(f"\n\n{asctime(localtime(time()))}____________________________________________________________\n")
         for message in message_list:
                 print(message)
-                file.write(message)
+                file.write(f"{message}\n")
         file.close()
 
 
